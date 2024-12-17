bfb3f24593f7f4beedee6e6e53b326c56059454b
Hari prasad <hariprasad9693@gmail.com>
2024-12-17 19:58:13 +0530

    cookiecutter template installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to predict the outcome of visa applications using machine learning. By analyzing various factors such as applicant's education, job experience, prevailing wages, and the company's profile, the model helps in making data-driven decisions about granting or denying visa requests. The project leverages machine learning models to predict the visa application status (Certified or Denied).',
+    author='Hari prasad',
+    license='',
+)
